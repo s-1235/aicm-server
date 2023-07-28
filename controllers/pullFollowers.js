a5b3b05db415ad3ca49a07ab3f71055a39b51506
s-1235 <sadamkhan505880@gmail.com>
2023-07-29 00:19:07 +0500

    Follwers Added

--- controllers/pullFollowers.js
+++ controllers/pullFollowers.js
@@ -2,9 +2,16 @@ const client = require('./../utils/twitterClient');
 
 exports.getFollowers = async (req, res) => {
     try {
-      const followers = await client.v2.followers(process.env.TWITTER_USER_ID, {
-        max_results: 5
-      });
+      const params = {
+        headers: {
+            'Authorization': 'Bearer '+ process.env.TWITTER_BEARER_TOKEN,
+        },
+        params: {
+            'user.fields': 'name,username',
+            'max_results': 5
+        }
+    };
+      const followers = await client.v2.followers(process.env.TWITTER_USER_ID, params);
       res.status(200).json(followers);
     } catch (error) {
       console.error('Error retrieving saved tweets:', error);
